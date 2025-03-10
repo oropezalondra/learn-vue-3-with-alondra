9fb31f652f95b00a675b654fb710b153b3b8300d
Alondra Oropeza <alondreza@gmail.com>
2025-03-10 18:34:41 -0500

    Component v-model for a twi-way data binding

--- src/BlogPost.vue
+++ src/BlogPost.vue
@@ -5,7 +5,15 @@
         </p>
 
         <h2>{{ id }} - {{ blogPostTitle }}</h2>
+        <div>
+            Modify the title: 
+            <input type="text" v-model="blogPostTitle" />
+        </div>
         <h4>{{ blogPostContent }}</h4>
+        <div>
+            modify the content: 
+            <input type="text" v-model="blogPostContent"/>
+        </div>
         <button @click="$emit('delete-blog-post', id)">Delete Post</button>
         <button @click="emitDeletePostEvent(id)">Delete Post</button>
     </div>
@@ -16,13 +24,16 @@ import {ref} from 'vue'
 
 let message = ref('This is is the BlogPost component.')
 
-defineProps(['id','blogPostTitle','blogPostContent'])
+defineProps(['id'])
 
  const emit = defineEmits(['delete-blog-post'])
 
 function emitDeletePostEvent(id) {
     emit('delete-blog-post', id)
 }
+
+let blogPostContent = defineModel('blogPostContent')
+let blogPostTitle = defineModel('blogPostTitle')
 
 </script>
 
@@ -32,6 +43,10 @@ function emitDeletePostEvent(id) {
     background-color: aqua;
     padding: 10px;
     margin-bottom: 10px;
+}
+
+input{
+    width: 100%;
 }
 
 </style>
